6e790f86b928eb088df05c34ea16717e6e4a38ab
Leo.Pirringer <leo.pirringer@sz-ybbs.ac.at>
2023-01-15 19:57:01 +0100

    second Release with JSON files

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,58 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        FileHandler fh = new FileHandler();
+        FileHandler fileH = new FileHandler();
 
-        WeatherReport eightDec = new WeatherReport("1. Jan.",26,"warm");
-        fh.writeToFile(eightDec);
+        WeatherReport firstJan = new WeatherReport("1. Jan.",26,"warm");
+        fileH.writeToFile(firstJan);
 
-        WeatherReport nineDec = new WeatherReport("2. Jan",18,"rather warm");
-        fh.writeToFile(nineDec);
+        WeatherReport secondJan = new WeatherReport("2. Jan",18,"rather warm");
+        fileH.writeToFile(secondJan);
 
-        WeatherReport tenDec = new WeatherReport("3. Jan",22," warm");
-        fh.writeToFile(tenDec);
+        WeatherReport thirdJan = new WeatherReport("3. Jan",22," warm");
+        fileH.writeToFile(thirdJan);
 
-        WeatherReport elevenDec = new WeatherReport("4. Jan",28,"Hot");
-        fh.writeToFile(elevenDec);
+        WeatherReport fourthJan = new WeatherReport("4. Jan",28,"Hot");
+        fileH.writeToFile(fourthJan);
 
-        WeatherReport twelveDec = new WeatherReport("5. Jan",35,"pretty hot");
-        fh.writeToFile(twelveDec);
+        WeatherReport fithJan = new WeatherReport("5. Jan",35,"pretty hot");
+        fileH.writeToFile(fithJan);
 
         JSONArray wr = new JSONArray();
 
         JSONObject weatherReportJSON = new JSONObject();
-        weatherReportJSON.put("date", eightDec.getDate());
-        weatherReportJSON.put("temperature", eightDec.getTemperature());
-        weatherReportJSON.put("summary", eightDec.getSummary());
+        weatherReportJSON.put("date", firstJan.getDate());
+        weatherReportJSON.put("temperature", firstJan.getTemperature());
+        weatherReportJSON.put("summary", firstJan.getSummary());
         wr.add(weatherReportJSON);
 
         weatherReportJSON = new JSONObject();
-        weatherReportJSON.put("date", nineDec.getDate());
-        weatherReportJSON.put("temperature", nineDec.getTemperature());
-        weatherReportJSON.put("summary", nineDec.getSummary());
+        weatherReportJSON.put("date", secondJan.getDate());
+        weatherReportJSON.put("temperature", secondJan.getTemperature());
+        weatherReportJSON.put("summary", secondJan.getSummary());
         wr.add(weatherReportJSON);
 
         weatherReportJSON = new JSONObject();
-        weatherReportJSON.put("date", tenDec.getDate());
-        weatherReportJSON.put("temperature", tenDec.getTemperature());
-        weatherReportJSON.put("summary", tenDec.getSummary());
+        weatherReportJSON.put("date", thirdJan.getDate());
+        weatherReportJSON.put("temperature", thirdJan.getTemperature());
+        weatherReportJSON.put("summary", thirdJan.getSummary());
         wr.add(weatherReportJSON);
 
         weatherReportJSON = new JSONObject();
-        weatherReportJSON.put("date", elevenDec.getDate());
-        weatherReportJSON.put("temperature", elevenDec.getTemperature());
-        weatherReportJSON.put("summary", elevenDec.getSummary());
+        weatherReportJSON.put("date", fourthJan.getDate());
+        weatherReportJSON.put("temperature", fourthJan.getTemperature());
+        weatherReportJSON.put("summary", fourthJan.getSummary());
         wr.add(weatherReportJSON);
 
         weatherReportJSON = new JSONObject();
-        weatherReportJSON.put("date", twelveDec.getDate());
-        weatherReportJSON.put("temperature", twelveDec.getTemperature());
-        weatherReportJSON.put("summary", twelveDec.getSummary());
+        weatherReportJSON.put("date", fithJan.getDate());
+        weatherReportJSON.put("temperature", fithJan.getTemperature());
+        weatherReportJSON.put("summary", fithJan.getSummary());
         wr.add(weatherReportJSON);
 
-        fh.writeToFile(wr);
-        List<WeatherReport> list = fh.readArrayFromFile();
+        fileH.writeToFile(wr);
+        List<WeatherReport> list = fileH.readArrayFromFile();
     }
 }
